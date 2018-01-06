614630483f5b7fcbdc5b27b6006833282e35086a
AustinKurtis <austin.jay.kurtis@gmail.com>
2018-01-06 13:55:30 -0600

    http services and http client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
-import { BrowserModule }        from '@angular/platform-browser';
 import { NgModule }             from '@angular/core';
+import { BrowserModule }        from '@angular/platform-browser';
 import { FormsModule }          from '@angular/forms';
-import { AppRoutingModule }     from './/app-routing.module';
-import { AppComponent }         from './app.component';
+import { HttpClientModule }     from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
+import { AppRoutingModule }     from './/app-routing.module';
+
+import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './dashboard/dashboard.component';
 import { HeroesComponent }      from './heroes/heroes.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 import { HeroService }          from './hero.service';
@@ -22,12 +27,20 @@ import { MessagesComponent }    from './messages/messages.component';
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+)
   ],
   providers: [ HeroService, MessageService ],
   bootstrap: [AppComponent]
